8bd39f9ac5f5f76cc7c52a087c2dc77dad7a3003
Johan <jsnider@users.noreply.github.com>
2020-09-15 16:09:42 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,7 @@ let star = game.createSprite(1, 1)
 for (let index = 0; index < 20; index++) {
     for (let index = 0; index < 2; index++) {
         star.move(1)
-        basic.pause(200)
     }
-    star.turn(Direction.Right, 162)
 }
 basic.forever(function () {
 	
